ce06f18fd13eebaf4d7ef920a9968980e504df5e
Timofei Bondarev <bondarevts@gmail.com>
2020-10-26 14:36:16 -0400

    Add ImagePicker class

---
+++ iPlateCounter/ImagePicker.swift
@@ -0,0 +1,54 @@
+// Code is based on
+// https://stackoverflow.com/a/63974045 by Di Nerd, CC BY-SA 4.0
+
+import SwiftUI
+import PhotosUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var images: [UIImage]
+    @Binding var showPicker: Bool
+    var selectionLimit: Int
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        config.selectionLimit = selectionLimit
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) { }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: ImagePicker
+        
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            
+            parent.showPicker.toggle()
+            
+            for img in results {
+                if img.itemProvider.canLoadObject(ofClass: UIImage.self) {
+                    img.itemProvider.loadObject(ofClass: UIImage.self) { (image, err) in
+                        guard let image1 = image else {return}
+                        
+                        DispatchQueue.main.async {
+                            self.parent.images.append(image1 as! UIImage)
+                        }
+                    }
+                } else {
+                    // Handle Error
+                    parent.showPicker.toggle()
+                }
+            }
+        }
+    }
+}

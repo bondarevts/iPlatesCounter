e7a90c27575622f710405bb412cadb36701fdae9
Timofei Bondarev <bondarevts@gmail.com>
2020-10-26 14:25:36 -0400

    Initial Commit

---
+++ iPlateCounter/iPlateCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPlateCounterApp.swift
+//  iPlateCounter
+//
+//  Created by tim on 10/26/20.
+//
+
+import SwiftUI
+
+@main
+struct iPlateCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
